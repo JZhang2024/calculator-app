b7db007f33961ea9e2db294b8188cd2ad78892cb
Jackie <jackie.zhang@uconn.edu>
2021-08-19 23:46:49 -0400

    Calculator can now compute and display all values with given operands and can reset with the 'all-clear' button. Click event listener has been refactored into a 'switch' statement instead of a bunch of 'if' statements.

--- script.js
+++ script.js
@@ -18,45 +18,114 @@ const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
   // Access the clicked element
   const { target } = event;
+  const { value } = target;
 
   // Check if the clicked element is a button.
   // If not, exit from the function
-  if (!target.matches('button')) {
-    return;
+  switch (value) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+      handleOperator(value);
+      break;
+    case '.':
+      inputDecimal(value);
+      break;
+    case 'all-clear':
+      resetCalculator();
+      break;
+    default:
+      // check if the key is an integer
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
   }
-
-  if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  inputDigit(target.value);
   updateDisplay();
 });
 
 function inputDigit(digit) {
-    const { displayValue } = calculator;
-    // Overwrite `displayValue` if the current value is '0' otherwise append to it
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
     calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
+
+  console.log(calculator);
+}
+
 function inputDecimal(dot) {
+  //checks if decimal starts second operand
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.';
+    calculator.waitingForSecondOperand = false;
+    return;
+  }
+  
   // If the `displayValue` property does not contain a decimal point
   if (!calculator.displayValue.includes(dot)) {
     // Append the decimal point
     calculator.displayValue += dot;
   }
 }
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+  //checks if operator has already been inputted and replaces with new operator if another is entered consecutively
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === '*') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+
+
 
   
 
